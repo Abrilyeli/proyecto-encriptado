6332e962a57c0aedee79a10c848af6813a9bf11e
Abrilyeli <abrilyeli@gmail.com>
2024-07-26 22:29:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
         loader.style.display = 'none';
         mainContent.style.display = 'flex';
-    }, 1500);
+    }, 2000);
 });
 
       
