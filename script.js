51a75a8bedcb0e711bca4ccd78555a4d6ddc077b
Abrilyeli <abrilyeli@gmail.com>
2024-07-29 11:40:01 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,137 +1,137 @@
 // duración animación de inicio
 document.addEventListener("DOMContentLoaded", function() {
-    const loader = document.getElementById('loader');
-    const mainContent = document.getElementById('mainContent');
-    
+    const cargador = document.getElementById('cargador');
+    const contenidoPrincipal = document.getElementById('contenidoPrincipal');
+
     setTimeout(() => {
-        loader.style.display = 'none';
-        mainContent.style.display = 'flex';
+        cargador.style.display = 'none';
+        contenidoPrincipal.display = 'flex';
     }, 2000);
 });
 
-      
+
 // Validar que el texto contenga solo letras minúsculas y espacios
-function validateInput(text) {
+function validarEntrada(texto) {
     const regex = /^[a-zñ0-9\s]+$/;
-    return regex.test(text);
+    return regex.test(texto);
 }
 
 // Mostrar mensaje de error
     let errorTimeout;
-    function showError(message) {
-    const errorMessage = document.getElementById('errorMessage');
-    errorMessage.textContent = message;
+    function mostrarError(mensaje) {
+    const mensajeError = document.getElementById('mensajeError');
+    mensajeError.textContent = mensaje;
     if (errorTimeout) {
         clearTimeout(errorTimeout);
     }
 
-    if (message) {
+    if (mensaje) {
         errorTimeout = setTimeout(() => {
-            errorMessage.textContent = '';
+            mesajeError.textContent = '';
         }, 2500);
     }
 }
 
 
 // Encriptar el texto
-    function encryptText() {
-    const inputTextElement = document.getElementById('inputText');
-    const inputText = inputTextElement.value.trim();
+    function encriptarTexto() {
+    const elementoTextoEntrada = document.getElementById('textoEntrada');
+    const textoEntrada = elementoTextoEntrada.value.trim();
 
-    if (inputText === "") {
-        showError('Por favor, ingrese un texto.');
+    if (textoEntrada === "") {
+        mostrarError('Por favor, ingrese un texto.');
         return;
     }
 
-    if (!validateInput(inputText)) {
-        showError('El texto solo debe contener letras minúsculas y espacios. No se permiten acentos ni  caracteres especiales.');
+    if (!validarEntrada(textoEntrada)) {
+        mostrarError('El texto solo debe contener letras minúsculas y espacios. No se permiten acentos ni  caracteres especiales.');
         return;
     }
 
-    const encryptedText = inputText
+    const textoEncriptado = textoEntrada
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
         .replace(/a/g, 'ai')
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
 
-    document.getElementById('outputText').value = encryptedText;
+    document.getElementById('textoSalida').value = textoEncriptado;
 
     // Borrar el texto del primer input
-    inputTextElement.value = '';
-    showError('');
+    elementoTextoEntrada.value = '';
+    mostrarError('');
 }
 
 // Desencriptar el texto
-function decryptText() {
-    const inputTextElement = document.getElementById('inputText');
-    const inputText = inputTextElement.value.trim();
+function desencriptarTexto() {
+    const elementoTextoEntrada = document.getElementById('textoEntrada');
+    const textoEntrada = elementoTextoEntrada.value.trim();
 
-    if (inputText === "") {
-        showError('Por favor, ingrese un texto.');
+    if (textoEntrada === "") {
+        mostrarError('Por favor, ingrese un texto.');
         return;
     }
 
-    if (!validateInput(inputText)) {
-        showError('El texto solo debe contener letras minúsculas y espacios. No se permiten acentos ni caracteres especiales.');
+    if (!validarEntrada(textoEntrada)) {
+        mostrarError('El texto solo debe contener letras minúsculas y espacios. No se permiten acentos ni caracteres especiales.');
         return;
     }
 
-    const decryptedText = inputText
+    const textoDesencriptado = textoEntrada
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
 
-    document.getElementById('outputText').value = decryptedText;
+    document.getElementById('textoSalida').value = textoDesencriptado;
 
     // Borrar el texto del primer input
-    inputTextElement.value = '';
-    showError('');
+    elementoTextoEntrada.value = '';
+    mostrarError('');
 }
 
 // Copiar el texto encriptado/desencriptado al portapapeles
-function copyText() {
-    const outputTextElement = document.getElementById('outputText');
-if (outputTextElement.value.trim() === "") {
-        showError('No hay texto para copiar.');
+function copiarTexto() {
+    const elementoTextoSalida = document.getElementById('textoSalida');
+if (elementoTextoSalida.value.trim() === "") {
+        mostrarError('No hay texto para copiar.');
         return;
     }
-  navigator.clipboard.writeText(outputTextElement.value)
+  navigator.clipboard.writeText(elementoTextoSalida.value)
         .then(() => {
-            document.getElementById('inputText').value = '';
-            document.getElementById('outputText').value = '';
-            showCopyMessage();
-            showError('');
+            document.getElementById('textoEntrada').value = '';
+            document.getElementById('Textosalida').value = '';
+            mostrarMensajeCopiado();
+            mostrarError('');
         })
         .catch(err => {
-            showError('Error al copiar el texto: ' + err);
+            mostrarError('Error al copiar el texto: ' + err);
         });
 }
 
 // Mostrar mensaje de copiado
-function showCopyMessage() {
-    const copyMessage = document.getElementById('copyMessage');
-    copyMessage.classList.add('show');
+function mostrarMemsajeCopiado() {
+    const mensajeCopiado = document.getElementById('mensajeCopiado');
+    mensajeCopiado.classList.add('mostrar');
     setTimeout(() => {
-        copyMessage.classList.remove('show');
+        mensajeCopiado.classList.remove('mostrar');
     }, 2000);
 }
 
 // Detectar el tipo de dispositivo
-function detectDevice() {
-    const deviceMessage = document.getElementById('deviceMessage');
-    const width = window.innerWidth;
+function detectarDispositivo() {
+    const mensajeDispositivo = document.getElementById('mensajeDispositivo');
+    const ancho = window.innerWidth;
 
-    if (width <= 600) {
-        deviceMessage.textContent = 'Estás usando un dispositivo móvil.';
-    } else if (width <= 1024) {
-        deviceMessage.textContent = 'Estás usando una tablet.';
+    if (ancho <= 600) {
+        mensajeDispositivo.textContent = 'Estás usando un dispositivo móvil.';
+    } else if (ancho <= 1024) {
+        mensajeDispositivo.textContent = 'Estás usando una tablet.';
     } else {
-        deviceMessage.textContent = 'Estás usando una computadora.';
+        mensajeDispositivo.textContent = 'Estás usando una computadora.';
     }
 }
 
-window.onload = detectDevice;
-window.onresize = detectDevice;
+window.onload = detectarDispositivo;
+window.onresize = detectarDispositivo;
